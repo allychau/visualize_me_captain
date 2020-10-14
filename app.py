ee48f43390ed3b9c0ba5bc6584d750b44d0c2cce
Allyson Chau <allyestuff@gmail.com>
2020-10-14 14:03:17 -0700

    correct version

--- app.py
+++ app.py
@@ -1,40 +1,54 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+import pandas as pd
+
+engine = create_engine("sqlite:///data/db.sqlite", echo=True)
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/db.sqlite'
-db = SQLAlchemy(app)
 
-db.Model.metadata.reflect(db.engine)
+#################### Flask routes #############################
 
-#class Station(db.Model):
-    #__tablename__ = 'Stations'
-    #__table_args__ = { 'extend_existing': True }
-    #LOC_CODE = db.Column(db.Text, primary_key=True)
-   
-
+# End point for landing page
 @app.route("/")
-def index(): 
-    #print("Total number of stations is", Station.query.count())
+def index():
     return render_template("index.html")
 
-@app.route("/leaflet")
-def leaflet():
-    return render_template("leaflet.html")
+#Route for map.  Charging stations markers and emission chloropleth
+@app.route("/map")
+def map():
+   return render_template("map.html")
 
+# Route get all the Electric Vehicle Charging Stations data.
+# Read data from Stations table in json format and display it to the browser.
 @app.route("/stations")
-def stations():
-    con = sql.connect("data/db.sqlite")
-    con.row_factory = sql.Row
-   
-    cur = con.cursor()
-    cur.execute("select * from Stations")
-   
-    rows = cur.fetchall()
-    return render_template("stations.html",rows = rows)
+def getAllStations():
+    conn = engine.connect()
+    query = f"select * from Stations"
+    df = pd.read_sql(query, conn)
+    stations = df.to_json(orient="records")
+    
+    return stations
+
+#@app.route('/chart')
+#def getBarChart():
+#def getLineChart():
+#    return jsonify(chart.html)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
